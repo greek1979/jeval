86d5f5fee9b45a902a5adfb118f54d46cd6604ab
Leonid Malikov <leonid@percival.co.uk>
2021-01-04 14:58:39 +0200

    Move generated javadocs to a branch

--- docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"net.sourceforge.jeval"},{"l":"net.sourceforge.jeval.function"},{"l":"net.sourceforge.jeval.function.math"},{"l":"net.sourceforge.jeval.function.string"},{"l":"net.sourceforge.jeval.operator"}]
